0bfc0f408fd9933275db7657278f9610ada282c0
uint64-dev <121733684+uint64-dev@users.noreply.github.com>
2022-12-31 23:23:19 +0100

    Release 1.0

---
+++ python-installer.sh
@@ -0,0 +1,4 @@
+clear
+echo "This installer comes with both Python 3 and a PIP (package manager). The installation should take no more than 5 minutes. Click any key to proceed with the installation."
+read input
+sudo apt install python3 python3-pip
